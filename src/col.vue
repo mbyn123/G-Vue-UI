e877d4fbe12b36753188ea8a045dc363b0dfe471
pan <371583920@qq.com>
2019-10-12 21:25:49 +0800

    添加row的align对齐方式

--- src/col.vue
+++ src/col.vue
@@ -1,8 +1,5 @@
 <template>
-    <div class="col" :class="colClass"
-         :style="colStyle"
-    >
-
+    <div class="col" :class="colClass" :style="colStyle">
         <div style="border: 1px solid green;height: 100px">
             <slot></slot>
         </div>
@@ -18,25 +15,22 @@
             offset: {
                 type: [Number, String]
             },
-            gutter: {
-                type: [Number, String]
-            }
         },
         data() {
             return {
-                gutter: 0
+                gutter:0
             }
         },
         computed: {
-            colClass(){
-                let {span,offset}=this
-                return [`col-${span}`, offset && `offset-${offset}`]
+            colClass() {
+                let {span, offset} = this
+                return [`col-${span}`, offset && `offset-${offset}`]//短路运算符 如果offset不存在就取后面的值
             },
             colStyle() {
-               return{
-                   paddingLeft:this.gutter/2+'px',
-                   paddingRight:this.gutter/2+'px'
-               }
+                return {
+                    paddingLeft: this.gutter / 2 + 'px',
+                    paddingRight: this.gutter / 2 + 'px'
+                }
             }
 
         }
@@ -45,7 +39,6 @@
 
 <style scoped lang="scss">
     .col {
-
         $class-perfix: col-;
         @for $n from 1 through 24 {
             &.#{$class-perfix}#{$n} {
--- src/row.vue
+++ src/row.vue
@@ -1,27 +1,37 @@
 <template>
-   <div class="row" :style="rowClass">
-       <slot></slot>
-   </div>
+    <div class="row" :style="rowStyle" :class="rowClass">
+        <slot></slot>
+    </div>
 </template>
 
 <script>
     export default {
-        props:{
-            gutter:{
-                type:[Number,String]
+        props: {
+            gutter: {
+                type: [Number, String]
+            },
+            align: {
+                type: String,
+                validator(value) {//设置传入的默认位置值 是否是这几个值
+                    return ['left', 'right', 'center'].includes(value)
+                }
             }
         },
         mounted() {
-            this.$children.forEach((vm)=>{
-                vm.gutter=this.gutter
+            this.$children.forEach((vm) => {
+                vm.gutter = this.gutter
             })
         },
-        computed:{
-            rowClass(){
+        computed: {
+            rowStyle() {
                 return {
-                    marginRight:-this.gutter/2+'px',
-                    marginLeft:-this.gutter/2+'px'
+                    marginRight: -this.gutter / 2 + 'px',
+                    marginLeft: -this.gutter / 2 + 'px',
                 }
+            },
+            rowClass() {
+                console.log('11111')
+                return [this.align && `align-${this.align}`]//短路运算符 如果align不存在就取后面的值
             }
         }
 
@@ -29,7 +39,17 @@
 </script>
 
 <style scoped lang="scss">
- .row{
-     display: flex;
- }
+    .row {
+        display: flex;
+        &.align-left{
+            justify-content:flex-start
+        }
+        &.align-right{
+            justify-content:flex-end;
+        }
+        &.align-center{
+            justify-content: center;
+        }
+
+    }
 </style>
