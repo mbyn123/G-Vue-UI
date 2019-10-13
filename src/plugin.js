a2a553b13dbac9e745b9bb5526879b52821928d5
pan <371583920@qq.com>
2019-10-13 23:07:55 +0800

    完成toast组件基本效果

--- src/plugin.js
+++ src/plugin.js
@@ -2,8 +2,17 @@ import Toast from './toast' //引入toast组件
 export default {
     install(Vue,options){
         Vue.prototype.$toast=function(message){
-           let Constructor = Vue.extend(Toast)
-           let toast = new Constructor()
+           let Constructor = Vue.extend(Toast)//继承toast组件
+           let toast = new Constructor({//创建一个构造函数,创建一个对象
+               propsData:{
+                   closeButton: {//取消按钮的信息
+                       text:'知道了',
+                       callback(){
+                           console.log('用户说他知道了')
+                       }
+                   }
+               }
+           })
             toast.$slots.default=[message]
             toast.$mount()
             document.body.appendChild(toast.$el)//生成一个toast组件放入body中
--- src/toast.vue
+++ src/toast.vue
@@ -1,11 +1,49 @@
 <template>
     <div class="toast">
         <slot></slot>
+        <div class="line"></div>
+        <span v-if="closeButton">{{closeButton.text}}</span>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        props:{
+            autoClose:{//提示信息是否自动消失
+                type:Boolean,
+                default:true
+            },
+            autoCloseDelay:{//自动消失的时间
+                type:Number,
+                default:3
+            },
+            closeButton:{//点击消失的按钮
+                type:Object,
+                default(){
+                    return{
+                        text:'关闭',
+                        callback:(toast)=>{
+                            toast.close()
+                        }
+                    }
+                }
+            }
+
+        },
+        methods:{
+            close(){//将内容从页面删除
+                this.$el.remove()
+                this.$destroy()
+            }
+        },
+        mounted(){
+            if(this.autoClose){//5秒后自动从页面删除
+                setTimeout(()=>{
+                    this.close()
+                },this.autoCloseDelay*1000)
+            }
+        },
+    }
 </script>
 
 <style scoped lang="scss">
@@ -27,5 +65,10 @@
         box-shadow: 0px 0px 3px 0px rgba(0,0,0,0.50);
         color: #fff;
         padding: 0 16px;
+    }
+    .line{
+        height:100%;
+        border:1px solid #666;
+        margin:0 8px;
     }
 </style>
