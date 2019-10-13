01538aefb5098ba5422a9ed1d4cf0370bce08250
pan <371583920@qq.com>
2019-10-13 18:08:10 +0800

    添加简单动画

--- src/sider.vue
+++ src/sider.vue
@@ -1,16 +1,38 @@
 <template>
-<div class="sider"><slot></slot></div>
+    <transition name="fade">
+    <div class="sider" v-if="hide">
+        <slot></slot>
+        <button @click="hide=false">hide</button>
+    </div>
+    </transition>
 </template>
 
 <script>
     export default {
-        name:'g-sider'
+        name: 'g-sider',
+        data() {
+            return {
+                hide: true
+            }
+        }
 
     }
 </script>
 
-<style scoped>
-.sider{
+<style scoped lang="scss">
+    .sider {
+        position: relative;
+    > button {
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
 
-}
+    }
+    .fade-enter-active, .fade-leave-active {
+        transition: all .5s;
+    }
+    .fade-enter, .fade-leave-to {
+        margin-left: -200px;
+    }
 </style>
